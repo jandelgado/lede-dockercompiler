ba609bfe21623d9522a4082e603f2f76a2ee9a29
Jan Delgado <jdelgado@gmx.net>
2020-04-17 08:51:23 +0200

    fix

--- example/example.sh
+++ example/example.sh
@@ -10,7 +10,11 @@ set -eou pipefail
 
 [ ! -d openwrt ] &&  git clone --depth 1 --branch v19.07.2 https://github.com/openwrt/openwrt
 
+pwd
+ls -l "$1"
 ln -sf "$1" openwrt/.config
+ls -l openwrt/.config
+exit 0
 cd openwrt
 
 ./scripts/feeds update -a
