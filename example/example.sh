e6105895a29d69c98b5e495651db42bb23cb7a40
Jan Delgado <jdelgado@gmx.net>
2020-04-17 08:59:56 +0200

    fix perm problems

--- example/example.sh
+++ example/example.sh
@@ -10,11 +10,7 @@ set -eou pipefail
 
 [ ! -d openwrt ] &&  git clone --depth 1 --branch v19.07.2 https://github.com/openwrt/openwrt
 
-pwd
-ls -l "$1"
-ln -sf "$1" openwrt/.config
-ls -l openwrt/.config
-exit 0
+cp "$1" openwrt/.config
 cd openwrt
 
 ./scripts/feeds update -a
