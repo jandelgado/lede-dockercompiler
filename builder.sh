a47bf678b39aa334bde60081747be52a43a98d5c
jandelgado <jandelgado@users.noreply.github.com>
2020-05-01 22:03:47 +0200

    New --docker-opts option (#4)
    new --docker-opts option
    use github actions, ditch travis
    new example

--- builder.sh
+++ builder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Docker based LEDE/OpenWRT build environment
-# (c) 2019 Jan Delgado
-set -e
+# (c) 2019-2020 Jan Delgado
+set -euo pipefail
 
 # base Tag to use for docker image
 DEF_IMAGE_TAG=jandelgado/openwrt-imagecompiler
@@ -9,27 +9,38 @@ IMAGE_TAG=${IMAGE_TAG:-$DEF_IMAGE_TAG}
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 WORK_DIR=$SCRIPT_DIR/workdir
 
-function usage_and_exit {
+function usage {
     cat<<EOT
 Dockerized OpenWRT compile environment.
 
 Usage: $1 COMMAND [OPTIONS] 
   COMMAND is one of:
-    build-docker-image- build docker image
-    shell             - start shell in docker container
+    build-docker-image    - build docker image
+    run CMD -- [ARGS...]  - run given command in docker container
+    shell                 - start shell in docker container
 
   OPTIONS:
-    -o WORK_DIR       - working directory (default $WORK_DIR)
-    --skip-sudo       - call docker directly, without sudo
+    -o WORK_DIR           - working directory (default $WORK_DIR)
+    --docker-opts OPTS    - additional options to pass to docker run
+                            (can occur multiple times)
+    --rootfs-overlay DIR  - rootfs overlay directory
+    --skip-sudo           - call docker directly, without sudo
 
 Environment:
-  IMAGE_TAG           - Tag to be used for docker image. 
-                        (default: $DEF_IMAGE_TAG)
+  IMAGE_TAG               - Tag to be used for docker image. 
+                            (default: $DEF_IMAGE_TAG)
 
 Example:
   ./builder.sh shell
+  ./builder.sh shell --docker-opts "-v=/tmp:/host-tmp"
+  ./builder.sh run -- sh -c "cd lede && make menuconfig defconfig world"
+ 
 EOT
-    exit 0
+}
+
+# return given file path as absolute path. path to file must exist.
+function abspath {
+    echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 }
 
 # build container 
@@ -39,49 +50,65 @@ function build_docker_image {
 }
 
 function run_cmd_in_container {
+    local docker_term_opts="-ti"
+    [ ! -t 0 ] && docker_term_opts="-i"
+    local rootfs_volume=()
+    if [ -n "${ROOTFS_OVERLAY+x}" ]; then
+        local rootfs
+        rootfs="$(abspath "$ROOTFS_OVERLAY")"
+        rootfs_volume=(-v "$rootfs":/rootfs-overlay:z)
+    fi
+    # shellcheck disable=SC2068
 	$SUDO docker run \
 			--rm \
 			-e GOSU_UID="$(id -ur)" -e GOSU_GID="$(id -g)" \
-            -v "$(cd "$WORK_DIR"; pwd)":/workdir:z \
-			-ti --rm "$IMAGE_TAG" "$@"
+            -v "$(abspath "$WORK_DIR")":/workdir:z \
+            "${rootfs_volume[@]}" \
+			$docker_term_opts \
+            --rm \
+            ${DOCKER_OPTS[@]} \
+            "$IMAGE_TAG" \
+            "$@"
 }
 
-# run a shell in the container, useful for debugging.
-function run_shell {
-    run_cmd_in_container bash
-}
-
-# print message and exit
 function fail {
     echo "ERROR: $*" >&2
     exit 1
 }
 
 if [ $# -lt 1 ]; then
-    usage_and_exit "$0"
+    usage "$0"
+    exit 0
 fi
 
 COMMAND=$1; shift
 SUDO=sudo
+DOCKER_OPTS=()
 
 # parse cli args, can override config file params
 while [[ $# -ge 1 ]]; do
     key="$1"
     case $key in
         -o) WORK_DIR="$2"; shift ;;
+        --rootfs-overlay) ROOTFS_OVERLAY="$2"; shift ;;
         --skip-sudo) SUDO="" ;;
+        --docker-opts) DOCKER_OPTS+=("$2"); shift ;;
+        --) shift; break ;;
         *) fail "invalid option: $key";;
     esac
     shift
 done
 
 [ ! -d "$WORK_DIR" ] && fail "output-dir: no such directory $WORK_DIR"
+[ -n  "${ROOTFS_OVERLAY+x}" ] && [ ! -d "$ROOTFS_OVERLAY" ] && fail "roofs-overlay: no such directory $ROOTFS_OVERLAY"
 
 case $COMMAND in
      build-docker-image) 
          build_docker_image  ;;
+     run)
+         run_cmd_in_container "$@" ;;
      shell) 
-         run_shell ;;
-     *) usage_and_exit "$0"
+         run_cmd_in_container bash ;;
+     *) usage "$0"; exit 0 ;;
 esac
 
---
+++ example/example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This script is run in the container and demonstrates a typical OpenWrt 
+# build session. Currently it is called from the ci.
+#
+# usage: example.sh CONFIG-FILE
+#   where CONFIG-FILE is an OpenWrt .config file
+
+set -eou pipefail
+
+[ ! -d openwrt ] &&  git clone --depth 1 --branch v19.07.2 https://github.com/openwrt/openwrt
+
+cp "$1" openwrt/.config
+cd openwrt
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+make defconfig
+make 
+
